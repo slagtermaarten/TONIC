a42fa2253642bc59bedf3beab8f3e7182b4dfe99
Maarten Slagter <slagter.maarten@gmail.com>
2018-02-02 18:57:02 +0100

    Init

--- R/load_data.R
+++ R/load_data.R
@@ -1,117 +1,37 @@
-## Load response data {{{
-harmonize_celllines <- function(vec) {
-  gsub('.CL_Untreated|\\.|-', '', vec)
-}
+library(data.table)
+# devtools::install('~/libs/maartenutils')
+library(maartenutils)
+library(dtplyr)
+library(tidyverse)
+p_root <- file.path('~/Projects/TONIC')
 
-fh <- fread(file.path('data-raw', 'cell_surface_data.csv'))
+patient_labels <- read.csv(file.path(p_root, 'data-raw/patient_labels.csv'),
+                           dec = ',', sep = ';') %>% as.data.table
+normalize_colnames(patient_labels)
+setnames(patient_labels, 'x', 'filename')
+setnames(patient_labels, gsub('\\.', '_', colnames(patient_labels))) 
+maartenutils::set_dt_types(patient_labels, 
+                           c('mean_log2_hk' = 'numeric', 
+                             'tis_score' = 'numeric'))
+patient_labels[, patient := paste0('pat_', patient)]
+setkey(patient_labels, filename)
 
-setnames(fh, c('cell_line', 
-               paste(rep(c('MHC', 'PDL1'), each = 4), 
-                     c('US', '0', '1', '10'), sep = "_")))
-fh <- fh[-1]
-num_cols <- setdiff(colnames(fh), 'cell_line')
-fh[, (num_cols) := lapply(.SD, function(x) as.numeric(gsub(',', '.', x))), 
-   .SDcols = num_cols]
-setkey(fh, cell_line)
+exp_levels <- read.csv(file.path(p_root, 'data-raw/normalized_gene_expression.csv'),
+                    # verbose = T,
+                    dec = ',', 
+                    sep = ';', skip = 1L) %>% as.data.table
 
-fh[, 'PDL1_FC_log2' := log2(PDL1_10) - log2(PDL1_0)]
-fh[, 'PDL1_FC' := 2^PDL1_FC_log2]
-# fh[, 'geom_mean' := (PDL1_0 * PDL1_10)^(1/2)]
-# fh[, 'harm_mean' := harmonic_mean(PDL1_0, PDL1_10)]
-fh[, cell_line := harmonize_celllines(cell_line)]
-## Global response variable to use in regression analyses
-resp_var <- 'PDL1_FC'
-## Load response data }}}
+setnames(exp_levels, gsub('.RCC$', '', colnames(exp_levels))) 
+setnames(exp_levels, gsub('^X', '', colnames(exp_levels))) 
+setnames(exp_levels, 'File.Name', 'gene_symbol')
+setdiff(colnames(exp_levels), 'gene_symbol') %>% 
+  { setNames(rep('numeric', length(.)), .) } %>%
+  set_dt_types(exp_levels, .)
+maartenutils::set_dt_types(exp_levels, 
+                           c('mean_log2_hk' = 'numeric', 
+                             'tis_score' = 'numeric'))
 
-## Load_transcriptome
-dat_path <- file.path('data-raw', 'RNAseq_data', 'DESeq2_normalized.RData')
-if (!exists('Sample_annotation') || !exists('RC_data')) 
-  load(dat_path)
-idx <- (is.na(Sample_annotation$Hours) | Sample_annotation$Hours == 24) & 
-       Sample_annotation$Treatment %in% c('Untreated')
-soi <- as.character(Sample_annotation$File_name[idx])
-Sample_names <- paste(Sample_annotation$Cell_line[idx], 
-                      Sample_annotation$Treatment[idx], sep = '_')
+# length(intersect(patient_labels[, filename], colnames(exp_levels)))
+# length(colnames(exp_levels))
 
-## Subselect to samples of interest and aggregate expression levels
-gene_var <- 'ensembl_gene_id'
-gene_var <- 'external_gene_id'
-RC_f <- RC_data[, c(gene_var, soi)]
-colnames(RC_f) <- c(gene_var, Sample_names)
-# rownames(RC_f) <- RC_f[, gene_var]
-if (any(table(RC_f[, gene_var]) > 1)) {
-  RC_f <- aggregate(formula = as.formula(sprintf('. ~ %s', gene_var)), 
-                    data = RC_f, 
-                    FUN = sum)
-  ## Some rows should have been combined and some columns unselected
-  stopifnot(all(dim(RC_f) < dim(RC_data)))
-}
-rownames(RC_f) <- RC_f[, gene_var]
-
-## Normalize expression data 
-if (F) {
-  ## Manual method
-  col_facs <- 1e-6 * apply(RC_f[, Sample_names], 2, sum)
-  RC_f_n <- scale(RC_f[, Sample_names], center = F, scale = col_facs) 
-  ## All column sums should equal 1e6
-  stopifnot(all(eps(apply(RC_f_n[, Sample_names], 2, sum), 1e6)))
-  ## No rows should have been lost
-  stopifnot(nrow(RC_f_n) == nrow(RC_f))
-  ## Name rows and columns
-  rownames(RC_f_n) <- as.character(RC_f[, gene_var])
-} else if (F) { 
-  ## Normalize with DESeq
-  ## Data already in CPM format?
-  colnames(RC_f)
-  rownames(RC_f)
-  gexp_counts <- RC_f
-  gexp_counts <- gexp_counts[, colSums(gexp_counts) > nrow(gexp_counts)]
-  print(dim(gexp_counts))
-} else {
-  RC_f_n <- RC_f
-}
-
-colnames(RC_f_n) <- harmonize_celllines(colnames(RC_f_n))
-if (F) {
-  inspect_mat(RC_f_n)
-  setdiff(harmonize_celllines(colnames(RC_f_n)[-1]),
-                              harmonize_celllines(fh[, cell_line]))
-}
-
-## Remove genes for which variance is 0, this confuses ggsea code
-idx <- which(!eps(apply(RC_f_n[, setdiff(colnames(RC_f_n), gene_var)], 
-                        1, sd), 1e-6))
-message(
-  format_frac(msg = 'RNA exp data genes removed with zero std', 
-              num = nrow(RC_f_n) - length(idx),
-              denom = nrow(RC_f_n)))
-RC_f_n <- RC_f_n[idx, ]
-rm(idx)
-
-## Handle technical duplicates
-if (F) {
-  ## Take mean of technical replicates
-  sample_names <- gsub('(.+)\\.1', '\\1', colnames(RC_f_n))
-  tech_means <- sapply(auto_name(unique(sample_names)), function(sn) {
-    idx <- which(sample_names == sn)
-    means <- apply(RC_f_n[, idx], 1, mean)
-    return(means)
-  })
-  RC_f_n <- tech_means
-  rm(sample_names)
-  rm(tech_means)
-} else {
-  ## Restrict data to experiments conducted in TS lab
-  sample_names <- gsub('(.+)\\.1', '\\1', colnames(RC_f_n))
-  ## Invert the boolean to select DP experiments instead
-  RC_f_n <- RC_f_n[, !duplicated(sample_names)]
-}
-
-## Select down to cell lines for which both expression and PD-L1 staining is
-## available
-shared_celllines <- intersect(harmonize_celllines(colnames(RC_f_n)[-1]),
-                              harmonize_celllines(fh[, cell_line]))
-RC_f_n <- RC_f_n[, colnames(RC_f_n) %in% shared_celllines]
-setkey(fh, cell_line)
-fh <- fh[shared_celllines]
-stopifnot(all(colnames(RC_f_n) == fh[, cell_line]))
+stopifnot(all(patient_labels[, filename] %in% colnames(exp_levels)))
