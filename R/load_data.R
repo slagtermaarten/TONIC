c3f14ac6f811a2d58a7dc37cfd520fbbcd131232
Maarten Slagter <slagter.maarten@gmail.com>
2018-02-05 17:20:47 +0100

    V1 of pairs plots
    Isolines suddenly not working anymore, gotta debug that

--- R/load_data.R
+++ R/load_data.R
@@ -4,6 +4,7 @@ library(maartenutils)
 library(dtplyr)
 library(tidyverse)
 p_root <- file.path('~/Projects/TONIC')
+img_dir < file.path(p_root, 'plots')
 
 patient_labels <- read.csv(file.path(p_root, 'data-raw/patient_labels.csv'),
                            dec = ',', sep = ';') %>% as.data.table
@@ -35,3 +36,28 @@ maartenutils::set_dt_types(exp_levels,
 # length(colnames(exp_levels))
 
 stopifnot(all(patient_labels[, filename] %in% colnames(exp_levels)))
+
+
+danaher_scores <- read.csv(file.path(p_root,
+                                     'data-raw/danaher_geneset_scores.csv'),
+                    # verbose = T,
+                    dec = ',', 
+                    sep = ';', skip = 0L) %>% as.data.table
+normalize_colnames(danaher_scores)
+
+## Ensure everything's numeric
+setdiff(colnames(danaher_scores), c('patient', 'arm', 'response')) %>% 
+  { setNames(rep('numeric', length(.)), .) } %>%
+  set_dt_types(danaher_scores, .)
+maartenutils::set_dt_types(exp_levels, 
+                           c('mean_log2_hk' = 'numeric', 
+                             'tis_score' = 'numeric'))
+danaher_scores.m <- melt(danaher_scores, 
+                         id.vars = c('patient', 'arm', 'response'))
+danaher_scores.m[, 'timepoint' := gsub('.*_(.*)', '\\1', variable)]
+danaher_scores.m[, variable := gsub('(.*)_.*', '\\1', variable)]
+danaher_scores.m[, patient := paste0('pat_', patient)]
+danaher_scores.m[, 'arm' := factor(arm, levels = levels(arm)[c(4,1,2,3,5)])]
+# danaher_scores.m <- merge(danaher_scores.m, 
+#                           patient_labels[, .(patient, response, arm)], 
+#                           by = 'patient')
