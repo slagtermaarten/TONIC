05e2e2f31da7201c879b3df91a983983076732cd
Maarten Slagter <slagter.maarten@gmail.com>
2018-07-10 23:07:48 +0200

    Bug fix in unpaired GSEA

--- R/GSEA_funcs_paired.R
+++ R/GSEA_funcs_paired.R
@@ -22,9 +22,9 @@ my_paired_FC_test <- function(x, y, abs = F) {
   ## Compute FCs per patient
   FCs <- rbindlist(lapply(unique(y$patient), function(patient) {
     pre_cf <- y[y$patient == parent.frame(3)$patient &
-                y$resp == 'Baseline', cf_number]
+                y$timepoint == 'Baseline', cf_number]
     post_cf <- y[y$patient == parent.frame(3)$patient &
-                 y$resp == 'Post-induction', cf_number]
+                 y$timepoint == 'Post-induction', cf_number]
     logFC <- unlist(x[match(post_cf, rownames(x)), ]) - unlist(x[match(pre_cf, rownames(x)), ])
     return(as.list(logFC))
   }), fill = T)
@@ -43,17 +43,17 @@ my_paired_WC_test <- function(x, y, abs = F) {
   cur_pats <- unique(y$patient)
   pre_idx <- vapply(cur_pats,
                     function(pat) which(y$patient == pat &
-                                        y$resp == 'Baseline'),
+                                        y$timepoint == 'Baseline'),
                     integer(1))
   post_idx <- vapply(cur_pats,
                      function(pat) which(y$patient == pat &
-                                         y$resp == 'Post-induction'),
+                                         y$timepoint == 'Post-induction'),
                      integer(1))
   ## TODO compute an exact WC here
   ret_val <- apply(x, 2, function(r) {
     w_test <- tryCatch(wilcox.test(x = r[pre_idx], y = r[post_idx], paired = T),
                        error = function(e) { print(e); browser() })
-    t_val <- sign(median(r[post_idx]) - median(r[pre_idx])) * 
+    t_val <- sign(median(r[post_idx]) - median(r[pre_idx])) *
       (1 - w_test$p.value)
     return(t_val)
   })
@@ -67,13 +67,44 @@ my_paired_WC_test <- function(x, y, abs = F) {
 }
 
 
+#' WC test ranking function
+#'
+#'
+my_unpaired_WC_test <- function(x, y, abs = F) {
+  R_idx <- y[, which(clinical_response == 'R')]
+  NR_idx <- y[, which(clinical_response == 'NR')]
+
+  # v <- x[, which(colnames(x) == 'FGR')]
+  # median(v[R_idx])
+  # median(v[NR_idx])
+
+  ret_val <- apply(x, 2, function(r) {
+    w_test <- tryCatch(wilcox.test(x = r[R_idx], y = r[NR_idx], paired = F),
+                       error = function(e) { print(e); browser() })
+    t_val <- sign(median(r[R_idx]) - median(r[NR_idx])) * (1 - w_test$p.value)
+    return(t_val)
+  })
+  ret_val <- as.matrix(ret_val)
+  colnames(ret_val) <- 'p_val'
+
+  if (abs) {
+    ret_val <- abs(ret_val)
+  }
+  return(ret_val)
+}
+
+
+
 #' Run GSEA
 #'
 #'
 gsea_wrapper <- function(gene_sets = HALLMARK_pathways,
                          patients = rna_sample_annotation[arm == 'Doxorubicin',
                                                           unique(patient)],
-                         resp_exp = 'timepoint',
+                         gene_score_fn = my_paired_WC_test,
+                         paired_test = T,
+                         resp_exp = NULL,
+                         allowed_timepoints = NULL,
                          nperm = 25,
                          abs = F,
                          fn_extra = '') {
@@ -81,17 +112,39 @@ gsea_wrapper <- function(gene_sets = HALLMARK_pathways,
 
   ## Prepare X and Y matrices
   resp_var <- rna_sample_annotation[patient %in% patients,
-                                    .(cf_number, 'resp' = get(resp_exp),
-                                      patient)]
+                                    .(patient, cf_number,
+                                      timepoint, clinical_response)]
   ## Subselect patients for which response var is not NA
-  resp_var <- resp_var[!is.na(resp)]
-  if (resp_var[, class(resp)] == 'factor') {
-    resp_var <- resp_var[, resp := droplevels(resp)]
+  if (!is.null(resp_exp)) {
+    resp_var <- resp_var[!is.na(get(resp_exp))]
+    if (resp_var[, class(get(resp_exp))] == 'factor') {
+      resp_var <- resp_var[, (resp_exp) := droplevels(get(resp_exp))]
+    }
   }
-  allowed_pats <- resp_var[, .N, by = patient][N == 2, patient]
-  if (length(allowed_pats) == 0) return(NULL)
-  resp_var <- resp_var[patient %in% allowed_pats]
-  design_mat <- stats::model.matrix(~ 0 + resp, as.data.frame(resp_var))
+
+  if (is.null(allowed_timepoints)) {
+    allowed_timepoints <- timepoints
+  }
+  resp_var <- resp_var[timepoint %in% allowed_timepoints]
+  if (null_dat(resp_var)) {
+    return(NULL)
+  }
+
+  if (paired_test && !null_dat(resp_var)) {
+    allowed_pats <- resp_var[, .N, by = patient][N == 2, patient]
+    if (length(allowed_pats) == 0) {
+      return(NULL)
+    }
+    resp_var <- resp_var[patient %in% allowed_pats]
+  }
+  if (null_dat(resp_var)) {
+    return(NULL)
+  }
+
+  ## As we're not using the variance stab, this is redundant and the formula
+  ## does not need to be consistent with the actual experiment
+  design_mat <- stats::model.matrix(as.formula(sprintf('~ 0 + timepoint')),
+                                    as.data.frame(resp_var))
   idx <- match(resp_var[, cf_number], colnames(rna_read_counts))
   RCs <- rna_read_counts[, idx, with = F]
   RCs$ensembl_gene_id <- rownames(rna_read_counts)
@@ -110,7 +163,7 @@ gsea_wrapper <- function(gene_sets = HALLMARK_pathways,
   res <- ggsea(x = cpms,
                y = resp_var,
                gene.sets = gene_sets,
-               gene.score.fn = my_paired_WC_test,
+               gene.score.fn = gene_score_fn,
                es.fn = ggsea_weighted_ks,
                sig.fun = ggsea_calc_sig,
                nperm = nperm,
@@ -143,29 +196,41 @@ gsea_wrapper <- function(gene_sets = HALLMARK_pathways,
 }
 
 
-gsea_all_arms <- function(resp_exp = 'timepoint', nperm = 10,
-                          abs = F, fn_extra = '') {
+gsea_all_arms <- function(gene_sets = HALLMARK_pathways,
+                          patients = rna_sample_annotation[!is.na(clinical_response),
+                                                           unique(patient)],
+                          allowed_timepoints = c('Baseline', 'Post-induction'),
+                          gene_score_fn = my_unpaired_WC_test,
+                          resp_exp = 'timepoint',
+                          paired_test = F,
+                          nperm = 1000,
+                          abs = F,
+                          fn_extra = '') {
   arm_specific_gsea <-
-    rbindlist(lapply(rna_sample_annotation[, levels(arm)], function(arm) {
-      res <- gsea_wrapper(gene_sets = HALLMARK_pathways,
-               patients = rna_sample_annotation[arm == parent.frame(3)$arm,
-                                                unique(patient)],
-               resp_exp = resp_exp,
-               nperm = nperm,
-               abs = abs,
-               fn_extra = fn_extra)
-      res$arm <- arm
+    rbindlist(lapply(c(rna_sample_annotation[, levels(arm)], 'All arms'),
+                     function(arm) {
+      l_patients <- rna_sample_annotation[patient %in% parent.frame(3)$patients,
+                                          patient]
+      if (arm != 'All arms') {
+        l_patients <- intersect(l_patients,
+                                rna_sample_annotation[arm == parent.frame(3)$arm,
+                                                      patient])
+      }
+      res <- gsea_wrapper(gene_sets = gene_sets,
+                          patients = l_patients,
+                          allowed_timepoints = allowed_timepoints,
+                          gene_score_fn = gene_score_fn,
+                          resp_exp = resp_exp,
+                          paired_test = paired_test,
+                          nperm = nperm,
+                          abs = abs,
+                          fn_extra = fn_extra)
+      if (!is.null(res))
+        res$arm <- arm
       return(res)
     }), fill = T)
 
-  res_u <- gsea_wrapper(gene_sets = HALLMARK_pathways,
-                        patients = rna_sample_annotation[, unique(patient)],
-                        resp_exp = resp_exp,
-                        nperm = nperm,
-                        abs = abs,
-                        fn_extra = fn_extra)
-  res_u$arm <- 'All arms'
-  res <- rbind(arm_specific_gsea, res_u, fill = T)
-  res$arm <- factor(res$arm, levels = c('All arms', treatment_arms))
-  return(res)
+  arm_specific_gsea$arm <- factor(res$arm, 
+                                  levels = c('All arms', treatment_arms))
+  return(arm_specific_gsea)
 }
